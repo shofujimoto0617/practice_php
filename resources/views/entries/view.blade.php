4d789be38cccb31a7d917399c9059d01194431fe
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-24 16:22:32 +0900

    put the code together in a partial template

--- resources/views/entries/view.blade.php
+++ resources/views/entries/view.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $entry->title }}</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', $entry->title . ' - Blog')
+
+@include('layouts.header')
+
+@section('content')
     <h1>{{ $entry->title }}</h1>
     <article>
     {{ $entry->content }}
     </article>
-</body>
-</html>
+@endsection
