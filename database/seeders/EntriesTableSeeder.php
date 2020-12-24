3c54c49092877e9a10318167dcbf70e654654b71
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-24 14:15:05 +0900

    add entries_table

---
+++ database/seeders/EntriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EntriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('entries')->insert([
+            'title'=>'entry01',
+            'content'=>'hello',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s')
+        ]);
+    }
+}
